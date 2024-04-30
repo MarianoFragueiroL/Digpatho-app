4e44900955deea9b8ef2490191cf8fedfb6a36c2
Mariano <m.fragueiro.lazcano@gmail.com>
2024-04-30 09:20:28 -0300

    Define interfaces

---
+++ src/types/updateki67file/interfaces.tsx
@@ -0,0 +1,14 @@
+export interface ImageData {
+    name: string;
+    original_image: string; // Base64 encoded string
+    data: string; // Base64 encoded string of the processed image
+    ki67: number;
+    positive: number;
+    total: number;
+    width: number;
+    height: number;
+  }
+  
+export interface UpdateFileProps {
+    onUpdateFile: (data: ImageData) => void;
+}
